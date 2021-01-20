a957c22cb305b78e28e4faea50d0a7c89c130293
Abhijith Kannan <abhijithkannan452@gmail.com>
2021-01-20 11:17:53 +0530

    update

--- React App/App.js
+++ React App/App.js
@@ -1,24 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { useFonts,Montserrat_400Regular,Montserrat_700Bold,} from '@expo-google-fonts/montserrat';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+// import { useFonts, Montserrat_400Regular, Montserrat_700Bold, } from '@expo-google-fonts/montserrat';
+import { TextInput, Button } from 'react-native-paper';
 
 
 
 export default function App() {
 
-  let {fontsLoaded,error} = useFonts({
-    regular:Montserrat_400Regular,
-    bold: Montserrat_700Bold
-  });
+  // let { fontsLoaded, error } = useFonts({
+  //   regular: Montserrat_400Regular,
+  //   bold: Montserrat_700Bold
+  // });
+  const [text, setText] = React.useState('');
+  const [password, setPassword] = React.useState('');
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.title}>Welcome Back !</Text>
-        <Text style={styles.subtitle}>Login to continue</Text>
+      <View style={{ width: "90%", height: "100%" ,backgroundColor:"blue"}}>
+
+
+        <View style={styles.textCon}>
+          <ImageBackground source={require("./assets/loginback.jpg")} style={styles.image}>
+            <Text style={styles.title}>Welcome Back !</Text>
+            <Text style={styles.subtitle}>Login to continue</Text>
+          </ImageBackground>
         </View>
-      <StatusBar style="auto" />
+
+        <View style={{ alignItems: 'center', justifyContent: 'center'}}>
+          <TextInput style={{ marginBottom: 20, width: "100%" }} label="Email" value={text} onChangeText={text => setText(text)} />
+          <TextInput style={{ marginBottom: 20, width: "100%" }} label="Password" value={password} secureTextEntry={true} onChangeText={password => setPassword(password)} />
+
+
+        </View>
+
+        <Text style={{ marginBottom: 20, }}>Forgot password ?</Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Button mode="contained" style={{ width: "100%" }} color="blue" onPress={() => console.log('Pressed')}> Sign In </Button>
+          <Text style={{ marginTop: 60, }}>Don't have an account yet ?</Text>
+          <Text>Sign Up</Text>
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -27,15 +51,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  title:{
-    fontFamily:"bold",
-    fontSize:24
+  textCon: {
+    height: "50%"
   },
-  subtitle:{
-    fontFamily:"regular",
-    fontSize:18
+  title: {
+    // fontFamily: "bold",
+    fontSize: 24,
+    color:"white"
+  },
+  subtitle: {
+    // fontFamily: "regular",
+    fontSize: 18,
+    color:"white"
+  },
+  image: {
+    width:"100%",
+    height:"100%",
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
