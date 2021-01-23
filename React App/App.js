25bc74bcca0fcdd8bcf50df639c888d9a4112a84
Abhijith Kannan <abhijithkannan452@gmail.com>
2021-01-23 14:51:30 +0530

    Update App.js

--- React App/App.js
+++ React App/App.js
@@ -1,43 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import Login from './components/login';
 import Routes from './routes/routes';
 import * as firebase from 'firebase';
-import {firebaseConfig} from './config';
+import { firebaseConfig } from './config';
 // firebase.initializeApp(firebaseConfig);
 
 
 export default function App() {
 
- const [login,setLogin] = useState(false);
-  
-useEffect(()=>{
-  if(!firebase.apps.length){
-    firebase.initializeApp(firebaseConfig);
-   
-    firebase.auth().onAuthStateChanged((user)=>{
-      if (!user) {
-      setLogin(false);
-      console.log("Signin")
-      }
-      else{
-        setLogin(true);
-        console.log("Logged in")
-      }
-      
-    })
-  }
-},[])
+  const [login, setLogin] = useState(false);
+
+  useEffect(() => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+
+      firebase.auth().onAuthStateChanged((user) => {
+        if (!user) {
+          setLogin(false);
+          console.log("Signin")
+        }
+        else {
+          setLogin(true);
+          console.log("Logged in")
+        }
+
+      })
+    }
+  }, [])
 
 
 
 
   return (
     <View style={styles.container}>
-      
-    {(login)?<Routes/>:<Login/>}
-      
+
+      {(login) ? <Routes /> : <Login />}
+
     </View>
   );
 }
@@ -46,7 +46,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    height:"100%"
+    height: "100%"
   },
- 
+
 });
